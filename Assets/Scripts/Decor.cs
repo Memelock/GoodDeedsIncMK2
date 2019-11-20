8d351198a5cb6a0785a5e7d3b1326b497cc90401
DavidKozdra <45106564+DavidKozdra@users.noreply.github.com>
2019-11-20 16:24:41 -0500

    win screen

---
+++ Assets/Scripts/Decor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decor : MonoBehaviour
+{
+    public Sprite[] Pics;
+    public float Time = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time > 0)
+        {
+            Time--;
+        }
+        else
+        {
+            Time = 2f;
+            gameObject.GetComponent<SpriteRenderer>().sprite = Pics[rand()];
+        }
+
+    }
+    int rand() {
+
+        int r = Random.Range(0, Pics.Length);
+        return r;
+    }
+}
---
+++ Assets/Scripts/Obstical.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstical : MonoBehaviour
+{
+    public static int Amount;
+    public Rigidbody2D Rb;
+    public float speed=2f;
+    private float direction=-1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Amount++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = direction;
+
+        Vector3 movement = new Vector3(0f,-1f, 0.0f);
+        Rb.velocity = movement * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Deleter") {
+            Amount--;
+            Destroy(gameObject);
+
+        }
+    }
+}
